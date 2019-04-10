61541f63c624d0c1f80f806257764c88a04933d3
XiaoYunhan <2028427786@qq.com>
2019-04-10 17:32:31 +0800

    add audio

--- main.cpp
+++ main.cpp
@@ -10,7 +10,28 @@
 #define PIN_MOTOR_LEFT_BACK 11
 #define PIN_CLOCK 9
 #define PIN_DATA 8
+#define PIN_BUZZER 5
 #define STACK_SIZE 200
+
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
 
 char blueToothValue;
 
@@ -77,9 +98,26 @@ void tGREENLED(void*p) {
 
 void tAudio(void*p) {
 	for(;;) {
-
+		scotlandTheBrave();
+		delay(250);
 	}
+}
 
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(PIN_BUZZER, note, duration);
+
+  
+    delay(duration);
+   
+  //Stop tone on buzzerPin
+  noTone(buzzerPin);
+ 
+  delay(50);
+ 
+  //Increment counter
+  counter++;
 }
 
 void tSerial(void*p) {
@@ -96,6 +134,7 @@ void setup() {
 	pinMode(PIN_MOTOR_LEFT_BACK,OUTPUT);
 	pinMode(PIN_CLOCK,OUTPUT);
 	pinMode(PIN_DATA,OUTPUT);
+	pinMode(PIN_BUZZER,OUTPUT);
 }
 
 void loop() {
@@ -105,4 +144,133 @@ void loop() {
 	xTaskCreate(tAudio, "Audio", STACK_SIZE, NULL, 3, NULL);
 	xTaskCreate(tSerial, "Serial", STACK_SIZE, NULL, 4, NULL);
 	vTaskStartScheduler();
+}
+
+//songs are below
+void scotlandTheBrave() {
+  beep(c, 1000);
+  beep(c, 750);    
+  beep(d, 250);
+  beep(e, 500);
+  beep(c, 500);  
+  beep(e, 500);
+  beep(g, 500);
+
+  beep(cH, 1000);
+  beep(cH, 750);    
+  beep(cH, 250);
+  beep(cH, 500);
+  beep(g, 500);  
+  beep(e, 500);
+  beep(c, 500);
+
+  beep(f, 1000);
+  beep(a, 750);    
+  beep(f, 250);
+  beep(e, 500);
+  beep(g, 500);  
+  beep(e, 500);
+  beep(c, 500);
+
+  beep(d, 1000);
+  beep(g, 750);    
+  beep(a, 250);
+  beep(g, 500);
+  beep(f, 500);  
+  beep(d, 500);
+  beep(c, 500);
+ 
+}
+void babyShark()
+{
+  beep(d, 300);
+  beep(e, 200);    
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);  
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);
+ 
+  delay(500);
+  
+  beep(d, 300);
+  beep(e, 200);    
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);  
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);
+ 
+  delay(500);
+
+  beep(d, 300);
+  beep(e, 200);    
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);  
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);
+  beep(g, 200);
+
+  delay(500);
+  beep(g, 300);
+  beep(g, 200);    
+  beep(370, 200);
+}
+ 
+void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);    
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);  
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+ 
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);  
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+}
+ 
+void secondSection()
+{
+  beep(aH, 500);
+  beep(a, 300);
+  beep(a, 150);
+  beep(aH, 500);
+  beep(gSH, 325);
+  beep(gH, 175);
+  beep(fSH, 125);
+  beep(fH, 125);    
+  beep(fSH, 250);
+ 
+  delay(325);
+ 
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);  
+  beep(cSH, 175);  
+  beep(cH, 125);  
+  beep(b, 125);  
+  beep(cH, 250);  
+ 
+  delay(350);
 }
